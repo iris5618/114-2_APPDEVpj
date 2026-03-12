311fed3e1dc4593ca1905e93860580d3c6f383d2
iris5618 <c112181144@nkust.edu.tw>
2026-03-12 11:10:41 +0800

    031202

--- week03/Triangle.java
+++ week03/Triangle.java
@@ -1,8 +1,32 @@
+class Car{
+   int wheel = 4;
+   int engine = 1;
+   String color = ="Gray";
+   void run(){}
+   void speedup?(){}
+}
+
 public class Triangle{
+    int edge=0;
+    int shape=0;
+    int height=0;
+    
     public static void main(String [] args){
-        printTriangle();
+        System.out.println("helloworld");
+        //printTriangle();
+        int total=0;
+        total = =sumN2N(1, 10);
+        System.out.println("total: " + total);
     }
 
+    static int sumN2N(int begin, int end){
+        int i=0;
+        int total=0;
+
+        for(i=begin; i<=end; i++){
+
+        }
+    }
 
 private static String str="";
 private static void printTriangle() {
