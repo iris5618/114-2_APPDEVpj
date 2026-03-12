e5a327e333d88f98ce4efc183d9733823ab4136c
iris5618 <c112181144@nkust.edu.tw>
2026-03-12 10:51:16 +0800

    031201

---
+++ week03/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle{
+    public static void main(String [] args){
+        printTriangle();
+    }
+
+
+private static String str="";
+private static void printTriangle() {
+    int i, j;
+    for ( i = 1; i <= 5; i++) {
+        for ( j = 1; j <= i; j++){
+            str += "*";
+            str += "\n";
+        }
+    }
+    System.out.println(str);
+}
+}
